0201f91d5df411b8a3b6235a79ed3f8568bb46c4
aidarbek.mambetaliev@gmail.com <aidarbek.mambetaliev@gmail.com>
2021-12-10 18:29:31 +0600

    added Product_Context

--- src/Components/Context/ProductContext.js
+++ src/Components/Context/ProductContext.js
@@ -1,57 +1,73 @@
-import React, { createContext, useReducer } from "react"; 
-import axios from "axios"; 
-export const productsContext = createContext(); 
-const API = "http://localhost:8000/products"; 
- 
-const INIT_STATE = { 
-  products: [], 
-  oneProduct: null, 
-}; 
- 
-function reducer(state = INIT_STATE, action) { 
-  switch (action.type) { 
-    case "GET_PRODUCTS": 
-      return { 
-        ...state, 
-        products: action.payload, 
-      }; 
-    case "GET_ONE_PRODUCT": 
-      return { 
-        ...state, 
-        oneProduct: action.payload, 
-      }; 
-    default: 
-      return state; 
-  } 
-} 
- 
-const ProductsContextProvider = ({ children }) => { 
-  const [state, dispatch] = useReducer(reducer, INIT_STATE); 
-  async function getProducts() { 
-    const result = await axios.get(API); 
-    dispatch({ type: "GET_PRODUCTS", payload: result.data }); 
-  } 
- 
-  async function getOneProduct(id) { 
-    const result = await axios.get(`${API}/${id}`); 
-    dispatch({ 
-      type: "GET_ONE_PRODUCT", 
-      payload: result.data, 
-    }); 
+import React, { createContext, useReducer } from "react";
+import axios from "axios";
+export const productsContext = createContext();
+const API = "http://localhost:8000/products";
+
+const INIT_STATE = {
+  products: [],
+  oneProduct: null,
+};
+
+function reducer(state = INIT_STATE, action) {
+  switch (action.type) {
+    case "GET_PRODUCTS":
+      return {
+        ...state,
+        products: action.payload,
+      };
+    case "GET_ONE_PRODUCT":
+      return {
+        ...state,
+        oneProduct: action.payload,
+      };
+    default:
+      return state;
   }
-  return ( 
-     
-    <productsContext.Provider 
-      value={{ 
-        products: state.products, 
-        oneProduct: state.oneProduct, 
-        getProducts, 
-        getOneProduct, 
-      }} 
-    > 
-      {children} 
-    </productsContext.Provider> 
-  ); 
-}; 
- 
+}
+
+const ProductsContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, INIT_STATE);
+  async function getProducts() {
+    const result = await axios.get(API);
+    dispatch({ type: "GET_PRODUCTS", payload: result.data });
+  }
+
+  async function getOneProduct(id) {
+    const result = await axios.get(`${API}/${id}`);
+    dispatch({
+      type: "GET_ONE_PRODUCT",
+      payload: result.data,
+    });
+  }
+
+  async function deleteProduct(id) {
+    await axios.delete(`${API}/${id}`);
+    getProducts();
+  }
+  async function createProduct(newProduct) {
+    await axios.post(API, newProduct);
+    getProducts();
+  }
+  async function updateProduct(id, editedProduct) {
+    await axios.patch(`${API}/${id}`, editedProduct);
+    getProducts();
+  }
+  return (
+    
+    <productsContext.Provider
+      value={{
+        products: state.products,
+        oneProduct: state.oneProduct,
+        getProducts,
+        getOneProduct,
+        deleteProduct,
+        createProduct,
+        updateProduct,
+      }}
+    >
+      {children}
+    </productsContext.Provider>
+  );
+};
+
 export default ProductsContextProvider;
