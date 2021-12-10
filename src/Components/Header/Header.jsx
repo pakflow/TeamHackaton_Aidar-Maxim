227c7b15f27791b6fedb181eba69e2581ef0641d
Maxim <bboy.mars.97@gmail.com>
2021-12-10 17:59:24 +0600

    install node and create header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from "../icons/logoH&M.png";
+import './Header.css';
+
+const Header = () => {
+    return (
+        <>
+        <div className="users__navbar">
+            navbar for users
+        </div>
+        <div className="navbar">
+            <img style={{width: "100px", height: "auto"}} src={logo} alt="logo" />
+        </div>
+        <nav>
+            <span>Женщины</span>
+            <span>Divided</span>
+            <span>Мужчины</span>
+            <span>Малыши</span>
+            <span>Дети</span>
+            <span>H&M HOME</span>
+            <span>Распродажа</span>
+        </nav>
+        </>
+    );
+};
+
+export default Header;
