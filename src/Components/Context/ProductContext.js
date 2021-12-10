7a742d97c63c01a6d072ae5611a0df30b0a16cfb
Maxim <bboy.mars.97@gmail.com>
2021-12-10 18:26:41 +0600

    create productcontext

---
+++ src/Components/Context/ProductContext.js
@@ -0,0 +1,57 @@
+import React, { createContext, useReducer } from "react"; 
+import axios from "axios"; 
+export const productsContext = createContext(); 
+const API = "http://localhost:8000/products"; 
+ 
+const INIT_STATE = { 
+  products: [], 
+  oneProduct: null, 
+}; 
+ 
+function reducer(state = INIT_STATE, action) { 
+  switch (action.type) { 
+    case "GET_PRODUCTS": 
+      return { 
+        ...state, 
+        products: action.payload, 
+      }; 
+    case "GET_ONE_PRODUCT": 
+      return { 
+        ...state, 
+        oneProduct: action.payload, 
+      }; 
+    default: 
+      return state; 
+  } 
+} 
+ 
+const ProductsContextProvider = ({ children }) => { 
+  const [state, dispatch] = useReducer(reducer, INIT_STATE); 
+  async function getProducts() { 
+    const result = await axios.get(API); 
+    dispatch({ type: "GET_PRODUCTS", payload: result.data }); 
+  } 
+ 
+  async function getOneProduct(id) { 
+    const result = await axios.get(`${API}/${id}`); 
+    dispatch({ 
+      type: "GET_ONE_PRODUCT", 
+      payload: result.data, 
+    }); 
+  }
+  return ( 
+     
+    <productsContext.Provider 
+      value={{ 
+        products: state.products, 
+        oneProduct: state.oneProduct, 
+        getProducts, 
+        getOneProduct, 
+      }} 
+    > 
+      {children} 
+    </productsContext.Provider> 
+  ); 
+}; 
+ 
+export default ProductsContextProvider;
